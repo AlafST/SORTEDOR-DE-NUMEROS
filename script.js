2023c4bc8271b0e09806f2646dfde8bf79935dc5
AlafST <alef-someday@hotmail.com>
2024-04-27 21:57:32 -0300

    Funcionalidade de sorteio adicionado, atualizado html css e javascript

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-function generateNumber(){
-
-    const min = Math.ceil(document.querySelector('.input-min').value)
-    const max =  Math.floor(document.querySelector('.input-max').value)
-   const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-   alert(result)
+function showNumberInputs(){
+    document.getElementById('numberInputs').style.display = 'block';
 }
 
+function generateNumber() {
+
+    const min = Math.ceil(document.querySelector("#numero-min").value); // valor do input "entre"
+    const max = Math.floor(document.querySelector("#numero-max").value); // valor do input "e"
+
+    //Verifica se min ou max são menores que zero
+    if (min < 0 || max < 0) {
+        document.querySelector('#resultdo-sorteio').textoContent = `Digite um número válido`;
+        return;
+    }
+
+    //Verifica se min é igual a max
+    if (min === max) {
+        document.querySelector('#resultado-sorteio').textContent = `Digite um número válido`;
+        return;
+    }
+
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        // Exibindo o resultado no <h3>
+        document.querySelector('#resultado-sorteio').innerHTML = `Número sorteado:<b> ${result} </b>`;
+       
+    }
+}
